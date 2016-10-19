8d23f2ef97c078fdc628f651ad40170acdff380e
mikal <mikal.callahan@icloud.com>
2016-10-19 12:54:23 -0700

    updates to some stuff

--- frontend/index.js
+++ frontend/index.js
@@ -1,26 +1,7 @@
-/*
- *	javascript for index (suggestions)
- */
-
-window.onload=function suggestions(){
-	var suggestedIngred = [	'Apple',
-							'Almond',
-							'Apricot',
-							'Artichoke',
-							'Asparagus',
-							'Avocado',
-							'Bacon',
-							'Baking Powder',
-							'Bay Leaf',
-							'Balsamic Vinegar',
-							'Cabbage',
-							'Dates',
-							'Eggs',
-							'Fennel'];
-	var list = document.getElementById('ingredients');
-	suggestedIngred.forEach(function suggestions(item){
-	   var option = document.createElement('option');
-	   option.value = item;
-	   list.appendChild(option);
-	};
+function createTable() {
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+	var entry = document.getElementById("foods").value;
+    cell1.innerHTML = entry;
 }
