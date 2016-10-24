28033c5a967feeaa92d48e019f80647ba53aba6b
mikal <mikal.callahan@icloud.com>
2016-10-24 12:33:14 -0700

    modified create account 100000x to try and get it to print json. Still doesn't work. modified user.json

--- frontend/createaccount.js
+++ frontend/createaccount.js
@@ -1,23 +1,12 @@
-$.fn.serializeObject = function()
-{
-    var o = {};
-    var a = this.serializeArray();
-    $.each(a, function() {
-        if (o[this.name] !== undefined) {
-            if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
-            }
-            o[this.name].push(this.value || '');
-        } else {
-            o[this.name] = this.value || '';
-        }
-    });
-    return o;
-};
-
-$(function() {
-    $('form').submit(function() {
-        $('#result').text(JSON.stringify($('form').serializeObject()));
-        return false;
-    });
+/*$(document).ready(function(){
+	 $("button").click(function(){
+		  var x = $("form").serializeArray();
+		  $.each(x, function(i, field){
+				$("#results").append(field.name + ":" + field.value + " ");
+		  });
+	 });
 });
+*/
+var formData = $("form.createaccount").serializeObject();
+console.log(formData);
+window.alert(formData);
