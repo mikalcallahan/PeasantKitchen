ff6ab2094dfe9a78e1d58ccbb2e2ad542fcf4b7e
mikal <mikal.callahan@icloud.com>
2016-11-28 13:09:10 -0800

    json stuff

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -3,12 +3,12 @@ ingredientsAmount = [];
 
 function createTable() {
     var ingredientstable = document.getElementById("ingrTable");
-    //var ingredientsstr = document.getElementById("ingrTable");
     var row1 = ingredientstable.insertRow(0);
     var cell1 = row1.insertCell(0);
     var entry = document.getElementById("foods").value;
     cell1.innerHTML = entry;
     ingredientsArray.push(entry);
+    document.getElementById("foods").value = ""; // clears out input field on enter
     alert(ingredientsstr);
 }
 
@@ -26,16 +26,48 @@ function submitIngredients() {
     ingredientsText = ingredientsArray.toString();
     alert(ingredientsText); // test output
 
-    $(function() {
-        $('#createaccount').submit(function() { // when submit is pressed
-            var request = ($('#createaccount').serializeObject()); //json-ify form data into jsonobject *put JSON.stringify right before ($)
-            var jsonobject = JSON.stringify({
-                id: "user.create",
-                payload: request
-            });
-            websockets(jsonobject); // call websockets() passing jsonobject
-        });
-    });
+    var requestParameters = {
+        ingredients: ingredientsArray,
+        username: "mah user"
+    };
+
+    var requestObject = {
+        id: "contains",
+        payload: requestParameters
+    };
+    requestObject = JSON.stringify(requestObject);
+    alert(requestObject);
+    websockets(requestObject);
+}
+
+/* web socket stuff */
+function websockets(jsonobject) {
+    /* if websocket is supported */
+    if ("WebSocket" in window) {
+        alert("WebSocket is supported by your Browser!"); // hurray its supported!
+        alert(jsonobject); // test to make sure jsonobject is passed
+        var ws = new WebSocket("ws://localhost:8080/Peasant_Kitchen/recipes"); // open websocket
+        alert("creating connection"); // creating connection
+
+        /* on websocket open */
+        ws.onopen = function() {
+            ws.send(jsonobject); // send jsonobject
+            alert("Message is sent..."); // json object sent
+        };
+
+        /* when backend receives data */
+        ws.onmessage = function(evt) {
+            var received_msg = evt.data; // received message is this
+            alert("Message is received..."); // we got the info
+        };
+
+        /* when websocket closes */
+        ws.onclose = function() {
+            alert("Connection is closed...");
+        };
+    } else { // browser doesn't support websockets
+        alert("WebSocket NOT supported by your Browser!");
+    }
 }
 
 function enterButton(e) {
