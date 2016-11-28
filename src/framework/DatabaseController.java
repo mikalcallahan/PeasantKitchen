dd15aa648f83282076e21a4f9278ad783b192561
gozack1 <go006@cougars.csusm.edu>
2016-11-28 21:45:33 +0000

    now

--- src/framework/DatabaseController.java
+++ src/framework/DatabaseController.java
@@ -18,9 +18,13 @@ public abstract class DatabaseController extends ObserverSubject
     public abstract User signInUser(String username) throws Exception;
 
     public abstract User signOutUser(String username) throws Exception;
+    
+    public abstract User removeUser(String username) throws Exception;
 
     //server related methods
     public abstract LifecycleEvent serverStartupTasks(LifecycleEvent startupEvent);
 
     public abstract LifecycleEvent serverShutdownTasks(LifecycleEvent shutdownEvent);
+    
+
 }
