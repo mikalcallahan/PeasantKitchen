8c85130284041808a1b958c1860914285f565409
Kyle Gullicksen <noobtastic101@gmail.com>
2016-12-02 12:11:43 -0800

    Comments for AddnSet

--- src/framework/AddnSet.java
+++ src/framework/AddnSet.java
@@ -3,21 +3,16 @@ package framework;
 import java.util.HashSet;
 
 /**
- * The type Addn set.
+ * Adds your item to the HashSet, and then returns the item you added to you
  *
  * @param <T> the type parameter
  */
 public class AddnSet<T> extends HashSet<T>
 {
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = 8085094930498135413L;
 
     /**
-     * Put t.
-     *
-     * @param item the item
+     * @param item the item you'd like to add
      * @return the t
      */
     public T put(T item)
